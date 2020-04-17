6039b6445b3009c0481348caa7bfbebfe0cc70a4
Robby Polana <rpolana@squarespace.com>
2020-04-17 18:46:33 -0400

    semantic ui express styled components

---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+// import React from "react";
+import App from "next/app";
+import "semantic-ui-css/semantic.min.css";
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Head from "next/head";
+
+export default () => (
+  <>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1>Hello World</h1>
+  </>
+);
