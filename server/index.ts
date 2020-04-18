d0425e68e8d7d02cfb88771006ddddd1e50193e9
Robby Polana <rpolana@squarespace.com>
2020-04-18 00:16:34 -0400

    all systems go

--- server/index.ts
+++ server/index.ts
@@ -2,20 +2,24 @@ import express, { Request, Response } from "express";
 import next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const nextServer = next({ dev });
+const handle = nextServer.getRequestHandler();
 const port = process.env.PORT || 3000;
+const host = "localhost";
 
 (async () => {
   try {
-    await app.prepare();
-    const server = express();
-    server.all("*", (req: Request, res: Response) => {
+    await nextServer.prepare();
+    const app = express();
+    app.get("/ping", (req, res) => res.json({ pong: true }));
+    app.all("*", (req: Request, res: Response) => {
       return handle(req, res);
     });
-    server.listen(port, (err?: any) => {
+    app.listen(port, (err?: any) => {
       if (err) throw err;
-      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+      console.log(
+        `> Ready on http://${host}:${port} - env ${process.env.NODE_ENV}`
+      );
     });
   } catch (e) {
     console.error(e);
